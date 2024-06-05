4db92a8424be0141c2af8ca9eb1ad5a39bc22b62
Mario Melendez <mariomelendez099@gmail.com>
2024-06-04 19:28:39 -0600

    Init the Structure of the Game

---
+++ game/game.py
@@ -0,0 +1,5 @@
+from mainLevel import initLevel
+
+# Conditional That Starts the Level.
+if __name__ == "__main__":
+    initLevel()
---
+++ game/src/universalFunctions.py
@@ -0,0 +1,22 @@
+from src import universalVariables as UV
+import raylib as ray
+
+#-------------------------------------------------------Init-Funcs Zone-------------------------------------------------------
+# Function to Create a Window or "Screen".
+def initScreen():
+    # Create the Window.
+    ray.InitWindow(UV.WIDTHSCREEN, UV.HEIGHTSCREEN, UV.screenName)
+
+    # Put a Config to "FULLSCREEN" to the Window.
+    ray.SetConfigFlags(ray.FLAG_FULLSCREEN_MODE)
+
+    
+
+#-------------------------------------------------------Draw-Funcs Zone-------------------------------------------------------
+# Function to Draw a Rectangle.
+def drawrec():
+    ray.DrawRectangle(UV.WIDTHSCREEN//2, UV.HEIGHTSCREEN//2, 50, 50, ray.RED)
+
+
+
+#-------------------------------------------------------Logic-Funcs Zone-------------------------------------------------------
